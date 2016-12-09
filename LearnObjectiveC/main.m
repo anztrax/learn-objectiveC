56df1a7223f96b7d554911fd4376617afc5e157d
andrew ananta <kuro.ketanitem@gmail.com>
2016-12-09 12:37:56 +0700

    Initial Commit

---
+++ LearnObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearnObjectiveC
+//
+//  Created by andrew ananta on 12/9/16.
+//  Copyright © 2016 andrew ananta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
